3c4c2fa7a6f48a27f3663bc08ebe4815b44b0849
nargok <nargok@gmail.com>
2020-09-28 09:58:01 +0900

    create reducer

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
+const initialState = { todos: [] };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add_todo":
+      const newTodos = [...state.todos];
+      newTodos.push({ id: state.todos.length + 1, task: action.payload });
+      return {
+        ...state,
+        todos: newTodos,
+      };
+    case "complete_task":
+      const filteredTodos = [...state.todos];
+      filteredTodos.splice(action.payload, 1);
+      return {
+        ...state,
+        todos: filteredTodos,
+      };
+    default:
+      throw new Error();
+  }
+};
+
 const Todo = () => <div>Hello</div>;
 
 export default Todo;
